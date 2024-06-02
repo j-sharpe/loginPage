4c92bdca5f49d8db0a8968e936228e4c26d6a851
Jayden Sharpe <jayden.mr.sharpe@gmail.com>
2024-06-02 13:48:43 -0400

    Logout, session cleanup on logout, email format validation

--- app/app.py
+++ app/app.py
@@ -1,34 +1,51 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask import Flask, render_template, request, flash, redirect, url_for, session, escape
 from flask_bcrypt import Bcrypt
-import sqlite3
+from email_validator import validate_email, EmailNotValidError
+import sqlite3, os
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
-app.config['SECRET_KEY'] = 'j48f903jo3ewrv9i324njkkfaerj5lk34r'
+app.config['SECRET_KEY'] = os.urandom(24).hex()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #Hashes password and checks that hashed pw is valid for a given password
-    if request.method == "POST":
-        user_exists = check_user_exists()
+    #Check if user session still active. Redirect since now sign in required.
+    if session.get('email') != None:
+        return redirect(url_for('welcome'))
 
+    #Handle post request from sign-in page form.
+    if request.method == "POST":
+
+        # Check format of user input email
+        try:
+            email_info = validate_email(request.form.get('userEmail', ''), check_deliverability=False)
+        except EmailNotValidError as e:
+            print(str(e))
+            flash("Invalid Email Address.")
+            return redirect(url_for('index'))
+
+        #Check that normalized email already in db --> shows user has account
+        user_exists = check_user_exists(email_info.normalized)
+
+        #Handle nonexisting user
         if user_exists == 0:
             flash("No Account Linked to this Email. Please sign-up for an account.")
-            return render_template('base.html', TestLogic=user_exists)
+            return render_template('base.html', TestLogic=request.form.get('userEmail'))
 
+        #Check hashed version of user input for password matches hashed password stored in db
         validate_password = check_password()
 
-        #return render_template('base.html', TestLogic=validate_password)
+        #Handle case where hashed user input doesn't match hashed pw in db
         if not validate_password:
             flash("Incorrect Password.")
             return render_template('base.html', TestLogic=validate_password)
 
+        #When user fully validated store email in session var and redirect to welcome user page
         session['email'] = request.form.get('userEmail', '')
         return redirect(url_for('welcome'))
 
-    session.pop('email', None)
-    print(session)
+    #Handle initial GET requests
     return render_template('base.html', TestLogic="unknown")
 
 @app.route('/welcome')
@@ -38,13 +55,20 @@ def welcome():
         return redirect(url_for('index'))
     return render_template('welcome.html', EMail=session['email'])
 
+@app.route('/logout')
+def logout():
+    flash("You have been logged out.")
+    session.pop('email')
+    return redirect(url_for('index'))
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-def check_user_exists():
-    user_email = request.form.get('userEmail', '')
+
+def check_user_exists(user_email:str):
+    #user_email = request.form.get('userEmail', '')
 
     conn = get_db_connection()
     user_exists_cursor = conn.execute('SELECT EXISTS (SELECT 1 FROM user_credentials WHERE email = ?)', (user_email,))
@@ -63,9 +87,9 @@ def check_password():
                                     (user_email,))
     stored_pw = stored_pw_cursor.fetchone()[0]
 
-    hashIsHash = bcrypt.check_password_hash(stored_pw, userPassword)
+    password_is_valid = bcrypt.check_password_hash(stored_pw, userPassword)
     conn.commit()
     conn.close()
 
-    return hashIsHash
+    return password_is_valid
 
