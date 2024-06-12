a6c48f8c59d2386e3bc4e2705e00361ea598ee83
Jayden Sharpe <jayden.mr.sharpe@gmail.com>
2024-06-12 12:27:03 -0400

    remove session variable for new user email

--- app/app.py
+++ app/app.py
@@ -22,14 +22,11 @@ client = Client(account_sid, auth_token)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #Check if user session still active. Redirect since now sign in required.
     if session.get('email') != None:
         return redirect(url_for('welcome'))
 
-    #Handle post request from sign-in page form.
     if request.method == "POST":
 
-        # Check format of user input email
         try:
             email_info = validate_email(request.form.get('userEmail', ''), check_deliverability=False)
         except EmailNotValidError as e:
@@ -37,27 +34,21 @@ def index():
             flash("Invalid Email Address.")
             return redirect(url_for('index'))
 
-        #Check that normalized email already in db --> shows user has account
         user_exists = check_user_exists(email_info.normalized)
 
-        #Handle nonexisting user
         if user_exists == 0:
             flash("No Account Linked to this Email. Please sign-up for an account.")
             return render_template('base.html', TestLogic=request.form.get('userEmail'))
 
-        #Check hashed version of user input for password matches hashed password stored in db
         validate_password = check_password()
 
-        #Handle case where hashed user input doesn't match hashed pw in db
         if not validate_password:
             flash("Incorrect Password.")
             return render_template('base.html', TestLogic=validate_password)
 
-        #When user fully validated store email in session var and redirect to welcome user page
         session['email'] = request.form.get('userEmail', '')
         return redirect(url_for('welcome'))
 
-    #Handle initial GET requests
     return render_template('base.html', TestLogic="unknown")
 
 @app.route('/welcome')
@@ -86,14 +77,11 @@ def verify_new_user():
             flash("Invalid Email Address.")
             return redirect(url_for('verify_new_user'))
 
-        #Check that normalized email already in db --> shows user has account
         user_exists = check_user_exists(email_info.normalized)
 
-        #Handle nonexisting user
         if user_exists == 1:
             flash("An account is already linked to this email. Please login.")
             return redirect(url_for('verify_new_user'))
-
 
         verification = client.verify\
                             .v2\
@@ -152,6 +140,7 @@ def create_password():
                 print(e)
                 return
             #redirect to login page
+            session.pop('newEmail')
             return redirect(url_for('index'))
         else:
             flash("Passwords don't match")
