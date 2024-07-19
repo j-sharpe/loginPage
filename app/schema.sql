eeea6048951e755e919526779207de9771bba998
Jayden Sharpe <jayden.mr.sharpe@gmail.com>
2024-07-19 14:13:21 -0400

    not null

--- app/schema.sql
+++ app/schema.sql
@@ -1,7 +1,15 @@
+DROP TABLE IF EXISTS user_information;
 DROP TABLE IF EXISTS user_credentials;
 
 CREATE TABLE user_credentials (
+    user_no INTEGER PRIMARY KEY NOT NULL,
     email VARCHAR(254),
     password VARCHAR(256),
-    PRIMARY KEY(email)
+);
+
+CREATE TABLE user_information (
+    FOREIGN KEY(user_no_id) REFERENCES user_credentials(user_no) ON DELETE CASCADE,
+    username VARCHAR(20),
+    f_name VARCHAR(30),
+    l_name VARCHAR(30),
 );
